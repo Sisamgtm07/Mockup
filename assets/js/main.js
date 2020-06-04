346d98a5d953d6d510418d4aa4707e1286d44a90
Sisamgtm07 <sisamgautam0001@gmail.com>
2020-06-04 21:11:16 +0545

    Complete

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+function menuToggle(){
+    let hamelement= document.getElementById("nav-list");
+    if(hamelement.style.display==="none"){
+        hamelement.style.display="block";
+    }
+    else
+    {
+        hamelement.style.display="none";
+    }
+}
+
